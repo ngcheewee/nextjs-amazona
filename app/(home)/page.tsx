16810a9d646fda47aca51e9233a71c117d4b62ff
Ng Chee Wee <ng_cheewee@yahoo.com>
2024-12-30 13:15:04 +0800

    03-create-website-layout

---
+++ app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  
+  return (
+    <div>
+      <h1 className="h1-bold text-center p-10 ">Home Page Content</h1>
+    </div>
+  );
+}
