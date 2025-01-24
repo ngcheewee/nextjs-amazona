facb8130179c5c5b2d5c1166d63283e80e72cf6c
Ng Chee Wee <ng_cheewee@yahoo.com>
2025-01-25 06:17:55 +0800

    11-Implement-Add-To-Cart

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,5 +1,5 @@
-import { ShoppingCartIcon } from 'lucide-react'
 import Link from 'next/link'
+import CartButton from './cart-button'
 export default function Menu() {
   return (
     <div className='flex justify-end'>
@@ -7,12 +7,8 @@ export default function Menu() {
         <Link href='/signin' className='flex items-center header-button'>
           Hello, Sign in
         </Link>
-        <Link href='/cart' className='header-button'>
-          <div className='flex items-end'>
-            <ShoppingCartIcon className='h-8 w-8' />
-            Cart
-          </div>
-        </Link>
+
+        <CartButton />
       </nav>
     </div>
   )
