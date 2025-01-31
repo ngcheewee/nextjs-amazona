553a74bb7beda3344062cc929b7868f209a4ba36
Ng Chee Wee <ng_cheewee@yahoo.com>
2025-02-01 07:46:30 +0800

    16-signin-with-google

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,22 +1,29 @@
 import { Metadata } from 'next'
 import Link from 'next/link'
 import { redirect } from 'next/navigation'
+
 import { auth } from '@/auth'
 import SeparatorWithOr from '@/components/shared/separator-or'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+
 import CredentialsSignInForm from './credentials-signin-form'
 import { Button } from '@/components/ui/button'
 import { APP_NAME } from '@/lib/constants'
+import { GoogleSignInForm } from './google-signin-form'
+
 export const metadata: Metadata = {
   title: 'Sign In',
 }
+
 export default async function SignIn(props: {
   searchParams: Promise<{
     callbackUrl: string
   }>
 }) {
   const searchParams = await props.searchParams
+
   const { callbackUrl = '/' } = searchParams
+
   const session = await auth()
   if (session) {
     return redirect(callbackUrl)
@@ -30,6 +37,10 @@ export default async function SignIn(props: {
         <CardContent>
           <div>
             <CredentialsSignInForm />
+          </div>
+          <SeparatorWithOr />
+          <div className='mt-4'>
+            <GoogleSignInForm />
           </div>
         </CardContent>
       </Card>
