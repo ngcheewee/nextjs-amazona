943c98be9fa1a11f7e9148dec27c5d0e3dbf7b82
Ng Chee Wee <ng_cheewee@yahoo.com>
2025-02-02 18:48:19 +0800

    18-place-order

--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,7 @@
 import {
   CartSchema,
   OrderItemSchema,
+  OrderInputSchema,
   ProductInputSchema,
   ShippingAddressSchema,
   UserInputSchema,
@@ -28,6 +29,7 @@ export type Data = {
   }[]
 }
 
+export type IOrderInput = z.infer<typeof OrderInputSchema>
 export type OrderItem = z.infer<typeof OrderItemSchema>
 export type Cart = z.infer<typeof CartSchema>
 export type ShippingAddress = z.infer<typeof ShippingAddressSchema>
