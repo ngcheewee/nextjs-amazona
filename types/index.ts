fc30cdfc253e39ccf652f3b768d9987daaf46663
Ng Chee Wee <ng_cheewee@yahoo.com>
2025-02-02 17:00:50 +0800

    17-create-checkout-page

--- types/index.ts
+++ types/index.ts
@@ -1,27 +1,36 @@
-import { CartSchema, OrderItemSchema, ProductInputSchema, UserInputSchema, UserSignInSchema, UserSignUpSchema } from "@/lib/validator";
+import {
+  CartSchema,
+  OrderItemSchema,
+  ProductInputSchema,
+  ShippingAddressSchema,
+  UserInputSchema,
+  UserSignInSchema,
+  UserSignUpSchema,
+} from '@/lib/validator'
 
-import { z } from "zod";
+import { z } from 'zod'
 
-export type IProductInput = z.infer<typeof  ProductInputSchema>;
+export type IProductInput = z.infer<typeof ProductInputSchema>
 
 export type Data = {
-    users: IUserInput[]
-    products: IProductInput[]
-    headerMenus: {
-      name: string
-      href: string
-    }[]
-    carousels: {
-      image: string
-      url: string
-      title: string
-      buttonCaption: string
-      isPublished: boolean
-    }[]
-  }
+  users: IUserInput[]
+  products: IProductInput[]
+  headerMenus: {
+    name: string
+    href: string
+  }[]
+  carousels: {
+    image: string
+    url: string
+    title: string
+    buttonCaption: string
+    isPublished: boolean
+  }[]
+}
 
 export type OrderItem = z.infer<typeof OrderItemSchema>
 export type Cart = z.infer<typeof CartSchema>
+export type ShippingAddress = z.infer<typeof ShippingAddressSchema>
 
 // user
 export type IUserInput = z.infer<typeof UserInputSchema>
