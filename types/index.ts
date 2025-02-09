2edee3fc6fb9071082599148c372250928c222ae
Ng Chee Wee <ng_cheewee@yahoo.com>
2025-02-09 18:33:02 +0800

    23-update-user-name

--- types/index.ts
+++ types/index.ts
@@ -8,6 +8,7 @@ import {
   UserInputSchema,
   UserSignInSchema,
   UserSignUpSchema,
+  UserNameSchema,
 } from '@/lib/validator'
 
 import { z } from 'zod'
@@ -52,3 +53,5 @@ export type ShippingAddress = z.infer<typeof ShippingAddressSchema>
 export type IUserInput = z.infer<typeof UserInputSchema>
 export type IUserSignIn = z.infer<typeof UserSignInSchema>
 export type IUserSignUp = z.infer<typeof UserSignUpSchema>
+export type IUserName = z.infer<typeof UserNameSchema>
+
