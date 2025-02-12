d1110320d049342100ca79ec466920299d4c123c
Ng Chee Wee <ng_cheewee@yahoo.com>
2025-02-12 22:35:28 +0800

    26-add-theme-color

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,13 +21,14 @@ export const metadata: Metadata = {
   },
   description: APP_DESCRIPTION,
 }
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
--- components/shared/client-providers.tsx
+++ components/shared/client-providers.tsx
@@ -3,14 +3,17 @@ import React from 'react'
 import useCartSidebar from '@/hooks/use-cart-sidebar'
 import CartSidebar from './cart-sidebar'
 import { Toaster } from '../ui/toaster'
+import { ThemeProvider } from './theme-provider'
+
 export default function ClientProviders({
   children,
 }: {
   children: React.ReactNode
 }) {
   const isCartSidebarOpen = useCartSidebar()
+  
   return (
-    <>
+    <ThemeProvider attribute='class' defaultTheme='system'>
       {isCartSidebarOpen ? (
         <div className='flex min-h-screen'>
           <div className='flex-1 overflow-hidden'>{children}</div>
@@ -20,6 +23,6 @@ export default function ClientProviders({
         <div>{children}</div>
       )}
       <Toaster />
-    </>
+    </ThemeProvider>
   )
 }
