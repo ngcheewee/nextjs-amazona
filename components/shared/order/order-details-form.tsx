f016038818d6fc39ed578e23e98473a9f50054f7
Ng Chee Wee <ng_cheewee@yahoo.com>
2025-02-20 07:28:03 +0800

    31-mark-orders-as-paid-delivered

--- components/shared/order/order-details-form.tsx
+++ components/shared/order/order-details-form.tsx
@@ -11,12 +11,17 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
+
 import { IOrder } from '@/lib/db/models/order.model'
 import { cn, formatDateTime } from '@/lib/utils'
 import { buttonVariants } from '@/components/ui/button'
 import ProductPrice from '../product/product-price'
+import ActionButton from '../action-button'
+import { deliverOrder, updateOrderToPaid } from '@/lib/actions/order.actions'
+
 export default function OrderDetailsForm({
   order,
+  isAdmin,
 }: {
   order: IOrder
   isAdmin: boolean
@@ -155,6 +160,19 @@ export default function OrderDetailsForm({
                 Pay Order
               </Link>
             )}
+            {isAdmin && !isPaid && paymentMethod === 'Cash On Delivery' && (
+              <ActionButton
+                caption='Mark as paid'
+                action={() => updateOrderToPaid(order._id)}
+              />
+            )}
+            
+            {isAdmin && isPaid && !isDelivered && (
+              <ActionButton
+                caption='Mark as delivered'
+                action={() => deliverOrder(order._id)}
+              />
+            )}            
           </CardContent>
         </Card>
       </div>
