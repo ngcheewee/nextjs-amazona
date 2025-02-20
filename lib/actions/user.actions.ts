dc1141069aed3726c054e9f046648286d61005a1
Ng Chee Wee <ng_cheewee@yahoo.com>
2025-02-20 21:52:48 +0800

    33-edit-user

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,14 +3,41 @@
 import bcrypt from 'bcryptjs'
 import { auth, signIn, signOut } from '@/auth'
 import { IUserName,IUserSignIn, IUserSignUp } from '@/types'
-import { UserSignUpSchema } from '@/lib/validator'
+import { UserSignUpSchema, UserUpdateSchema } from '../validator'
 import { redirect } from 'next/navigation'
 import { connectToDatabase } from '../db'
 import { formatError } from '../utils'
 import User, { IUser } from '../db/models/user.model'
 import { revalidatePath } from 'next/cache'
 import { PAGE_SIZE } from '../constants'
+import { z } from 'zod'
 
+export async function updateUser(user: z.infer<typeof UserUpdateSchema>) {
+  try {
+    await connectToDatabase()
+    const dbUser = await User.findById(user._id)
+    if (!dbUser) throw new Error('User not found')
+    dbUser.name = user.name
+    dbUser.email = user.email
+    dbUser.role = user.role
+    const updatedUser = await dbUser.save()
+    revalidatePath('/admin/users')
+    return {
+      success: true,
+      message: 'User updated successfully',
+      data: JSON.parse(JSON.stringify(updatedUser)),
+    }
+  } catch (error) {
+    return { success: false, message: formatError(error) }
+  }
+}
+
+export async function getUserById(userId: string) {
+  await connectToDatabase()
+  const user = await User.findById(userId)
+  if (!user) throw new Error('User not found')
+  return JSON.parse(JSON.stringify(user)) as IUser
+}
 // DELETE
 
 export async function deleteUser(id: string) {
